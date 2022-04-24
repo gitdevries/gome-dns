cd949d35e9c0edfb66c5eef67a5490d4e91d0bc2
Joey de Vries <13612647+gitdevries@users.noreply.github.com>
2022-04-24 23:33:18 +0200

    Optimized server to not use dns2 but more native like approach

---
+++ src/DnsResolver.ts
@@ -0,0 +1,40 @@
+import Resolver from './resolvers';
+
+import * as dns from 'dns';
+import { Packet } from 'dns-packet';
+
+export default new class DnsResolver {
+    constructor(resolver: Resolver = Resolver.CLOUDFLARE) {
+        dns.setServers([resolver]);
+    }
+
+    private id(min: number, max: number) {
+        return Math.floor(Math.random() * (max - min + 1)) + min
+    }
+
+    private gomeResolve() {
+
+    }
+
+    public parse(message: Packet): Packet {
+        console.log(message);
+        const [q] = message.questions ?? [];
+
+        // TODO: Use resolve to respond instead of hardcode
+        dns.resolve(q.name, q.type, (addresses) => {
+
+        });
+
+        return {
+            type: 'response',
+            id: message.id ?? this.id(0, 65534),
+            answers: [{
+                name: q.name,
+                // type: q.type,
+                type: 'A',
+                class: q.class,
+                data: "127.0.0.1"
+            }]
+        };
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,52 @@
 import logger from './lib/logger';
-import dns from './lib/dns';
+import DnsResolver from './DnsResolver';
 
-import { createServer, DnsRequest, DnsResponse } from 'dns2';
-import { RemoteInfo } from 'dgram';
+import { createSocket, RemoteInfo } from 'dgram';
+import { decode, encode, Packet } from 'dns-packet';
 
-const server = createServer({ udp: true, handle });
+const socket = createSocket('udp4');
 
-function handle(request: DnsRequest, send: (response: DnsResponse) => void, rinfo: RemoteInfo) {
-    const response = dns.resolve(request, rinfo);
-    send(response);
-}
-
-server.on('listening', () => {
-    logger.info(`Server started on :8053`);
+socket.on('error', (err) => {
+    console.log(`server error:\n${err.stack}`);
+    socket.close();
 });
 
-server.on('close', () => {
-    logger.info(`Server shutdown`);
+socket.on('message', (msg: Buffer, rinfo: RemoteInfo) => {
+    logger.info(`Incomming request from ${rinfo.address}:${rinfo.port}`);
+    const message: Packet = decode(msg);
+    const response: Packet = DnsResolver.parse(message);
+
+    const buf = new Buffer(encode(response));
+    socket.send(buf, 0, buf.length, rinfo.port, rinfo.address)
 });
 
-server.listen({
-    udp: 8053
+socket.on('listening', () => {
+    const address = socket.address();
+    logger.info(`Socket server started on ${address.address}:${address.port}`);
 });
 
-process.on('SIGTERM', () => {
-    logger.info('SIGTERM signal received: closing HTTP server');
-    server.close();
-});
+socket.bind(8053);
+
+// const server = createServer({ udp: true, handle });
+
+// function handle(request: DnsRequest, send: (response: DnsResponse) => void, rinfo: RemoteInfo) {
+//     const resolver = new DnsResolver(request, rinfo);
+//     send(resolver.getResponse());
+// }
+
+// server.on('listening', () => {
+//
+// });
+
+// server.on('close', () => {
+//     logger.info(`Server shutdown`);
+// });
+
+// server.listen({
+//     udp: 8053
+// });
+
+// process.on('SIGTERM', () => {
+//     logger.info('SIGTERM signal received: closing HTTP server');
+//     server.close();
+// });
